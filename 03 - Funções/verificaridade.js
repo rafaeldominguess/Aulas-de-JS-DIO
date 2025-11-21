946ddfee1037d4a6077a974ac6981e43a23f7c6e
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2025-11-21 10:06:47 -0300

    Verificador se é maior de idade
    Exercício para verificar se é maior ou menos de idade.

---
+++ 03 - Funções/verificaridade.js
@@ -0,0 +1,10 @@
+
+function verificadorDeIdade(idade) {
+    if (idade >= 18) {
+        return ('maior de idade');
+    } else {
+        return ('menor de idade')
+    }
+}
+
+console.log('Você é ' + verificadorDeIdade(18));
