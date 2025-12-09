f0f30414fb7211c2f0fdbc08d47f4f7168d91cff
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2025-12-09 20:11:44 -0300

    calculando IMC
    calculando IMC com métodos

--- 04 - Objetos/classepessoa.js
+++ 04 - Objetos/classepessoa.js
@@ -20,12 +20,28 @@ class Pessoas {
         return (this.peso / (this.altura * this.altura)).toFixed(2);
     }
 
+    classificarIMC() {
+        const imc = this.calcularIMC();
+        if (imc < 18.5) {
+            return ('Abaixo do peso');
+        } else if (imc >= 18.5 && imc < 25) {
+            return ('Peso normal');
+        } else if (imc >= 25 && imc < 30) {
+            return ('Acima do peso');
+        } else if (imc >= 30 && imc < 40) {
+            return ('Obeso');
+        } else {
+            console.log('Obesidade grave');
+        }
+    }
+
 }
 
 
-const oscar = new Pessoas('Oscar',70, 1.75);
-const rafael = new Pessoas('Rafael',78, 1.70);
+const oscar = new Pessoas('Oscar', 100, 1.75);
+const rafael = new Pessoas('Rafael', 78, 1.70);
 
 
-console.log(oscar, `Meu IMC é: ${oscar.calcularIMC()}` );
-console.log(rafael, `Meu IMC é: ${rafael.calcularIMC()}` );
+console.log(oscar, `Meu IMC é: ${oscar.calcularIMC()}`,` ${oscar.classificarIMC()}`);
+console.log(rafael, `Meu IMC é: ${rafael.calcularIMC()}`,` ${rafael.classificarIMC()}`);
+
