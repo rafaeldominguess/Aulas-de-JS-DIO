d722afacab3189889e8b68d146dabaaf8c46d258
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2025-12-09 20:33:46 -0300

    Arrays
    aprendendo a usar listas

---
+++ 05 - Arrays/aula.js
@@ -0,0 +1,2 @@
+const alunos = ['Rafael','Oscar','Lucieli','Ana','Antonio',];  /* Lista, sempre começa na  */
+    console.log(alunos[0]);                                    /*   posição zero [0] */
