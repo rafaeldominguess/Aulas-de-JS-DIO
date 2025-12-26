eba7591b2c0af642620b7ad6c8a07e60f5286e7b
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2025-12-26 19:03:42 -0300

    Conhecendo alguns tipos  de dados

---
+++ tipos de dados/tipos-dados.js
@@ -0,0 +1,29 @@
+
+
+/*
+    boolean 
+    null
+    undefined
+    number
+    string
+    symbol
+*/
+
+// Object 
+
+console.log( 10 == '10' ); //comparação não levando em consideração number e string (tipo).
+// 10 é igual a 10 independente se é número ou string.
+
+console.log( 10 === '10'); // comparação levando em consideração number e string (tipo).
+// 10 é diferente de '10', número não é igual a string.
+
+// boolean = true and false
+
+console.log(!'')
+
+
+//String
+
+"teste" // Pouco utilizado 
+'teste' // Mais utilizado
+`teste` // Utilizado atualmente pq pode colocar um template ${value}.
