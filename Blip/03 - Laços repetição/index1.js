22399b2567651e15104e2c716909f5031b560f03
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-01-11 20:33:35 -0300

    feat: Simulando um personagem tomando uma poção de vida e aumentando a vida, utilizando for para isso.

---
+++ Blip/03 - Laços repetição/index1.js
@@ -0,0 +1,10 @@
+//While simulando um personagem tomando poção de vida
+
+let pontosDeVida = 0
+
+console.log("Vida do personagem: " + pontosDeVida)
+
+for (let i = 1; i <= 10; i++) {
+    console.log("Tomou a poção de vida!" )
+    console.log("Vida do personagem: " + i)
+}
