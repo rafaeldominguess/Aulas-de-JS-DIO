d8ab789101ebbfb0260807ff115ad5aac0fdc903
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-01-11 20:48:20 -0300

    feat: Solucionando um exercício em que tenho uma lista de números e preciso imprimir apenas os números pares.

---
+++ Blip/03 - Laços repetição/index2.js
@@ -0,0 +1,16 @@
+//While, pegando números pares de um array.
+
+const numeros = [0, 1, 2, 3, 5, 7, 10, 4, 20, 12]
+
+
+console.log("Os números pares são: ")
+
+
+for (let i = 0; i < numeros.length; i++) {
+    const numerosPares = numeros[i];
+
+    if (numerosPares % 2 === 0) {
+       console.log(numerosPares)
+    }
+}
+ 
