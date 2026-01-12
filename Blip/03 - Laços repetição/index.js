a649b6e56b4d46b78ea97caeb2a89303e4fb3584
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-01-11 21:32:00 -0300

    feat: Ajustes nos códigos.

--- Blip/03 - Laços repetição/index.js
+++ Blip/03 - Laços repetição/index.js
@@ -4,8 +4,8 @@
 let tabela = [0, 2, 4, 6, 8, 10]
 
 
-for (let numeros = 0; numeros < tabela.length; numeros++) {
-    const element = tabela[numeros];
+for (let i = 0; i < tabela.length; i++) {
+    const element = tabela[i];
     console.log(element)
 }
 
