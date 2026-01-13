4785408e958be058b0cf3a2e92934bfb70d26785
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-01-13 16:29:23 -0300

    feat: Finalizando desafio Blip

---
+++ Blip/05 - Funções/index3.js
@@ -0,0 +1,45 @@
+
+
+
+function calcularNivel(quantidadeVitorias, quantidadeDerrotas) {
+
+    let saldoVitorias = quantidadeVitorias - quantidadeDerrotas
+    let nivel = ""
+
+    if (saldoVitorias < 11) {
+        nivel = "Ferro"
+    } else if (saldoVitorias < 21) {
+        nivel = "Bronze"
+    } else if (saldoVitorias < 51) {
+        nivel = "Prata"
+    } else if (saldoVitorias < 81) {
+        nivel = "Ouro"
+    } else if (saldoVitorias < 91) {
+        nivel = "Diamante"
+    } else if (saldoVitorias < 101) {
+        nivel = "Lendário"
+    } else {
+        nivel = "Imortal"
+    }
+
+    return `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`
+}
+
+
+
+const listaDePartidas = [
+    [90,10],
+    [200,10],
+    [10,2]
+]
+
+for (let i = 0; i < listaDePartidas.length; i++) {
+    let resultado = calcularNivel(listaDePartidas[i][0],listaDePartidas[i][1])
+    console.log(resultado)
+}
+
+
+
+
+
+
