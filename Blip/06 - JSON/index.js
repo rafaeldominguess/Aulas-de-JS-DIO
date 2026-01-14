211d58c2b44545e14354249272fff304995e7e2d
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-01-14 15:51:04 -0300

    feat: Estudando JSON e completando um desafio, onde eu tenho um objeto e preciso imprimir o resultado através de um for in para buscar o valores.

---
+++ Blip/06 - JSON/index.js
@@ -0,0 +1,28 @@
+let dataClient = { // Objetos
+    name: "Rafael",
+    age: 26,
+    products: {
+        0: ["Mouse", 49.90],
+        1: ["Teclado", 180.90],
+        2: ["Monitor", 899.90],
+        3: ["Mousepad", 30.90]
+    }
+}
+
+
+
+function generateDataCliente(dataClient) {
+    console.log(`Nome do cliente é ${dataClient.name}, idade ${dataClient.age} anos, os produtos são:`)
+
+    //For in percorre toda a lista da minha variável com objetos.
+    for (let i in dataClient.products) {
+        let [productName, productPrice] = dataClient.products[i]
+        console.log(`${productName}: R$ ${productPrice.toFixed(2)}`)
+    }
+}
+
+generateDataCliente(dataClient)
+
+//API Rest = transferir dados de um sistema para outro.
+//Protocolo de comunicação entre sistemas.
+
