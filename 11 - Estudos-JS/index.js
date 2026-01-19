70cc2b09e8043168bb5a61b0e6498b4eefa308db
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2026-01-19 15:30:51 -0300

    feat: Adiciona arquivo index.js com exemplos de manipulação de strings

---
+++ 11 - Estudos-JS/index.js
@@ -0,0 +1,26 @@
+const nome = "Rafael Vera Domingues"
+
+nome.length // Quantidade de caracteres da string
+console.log(nome.length)
+
+nome.split(" ") // Divide a string em um array de strings, usando o separador especificado
+console.log(nome.split(" "))
+
+nome.toUpperCase() // Converte a string para letras maiúsculas
+console.log(nome.toUpperCase())
+
+nome.toLowerCase() // Converte a string para letras minúsculas
+console.log(nome.toLowerCase())
+
+nome.includes("Rafael") // Verifica se a string contém a substring especificada
+console.log(nome.includes("Rafael"))
+
+nome.slice(0, 6) // Extrai uma parte da string entre os índices especificados
+console.log(nome.slice(0, 6))
+
+nome.replace("Rafael", "Sr.") // Substitui uma parte da string por outra
+console.log(nome.replace("Rafael", "Sr."))
+
+
+
+
